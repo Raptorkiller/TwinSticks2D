5e9737f27cb91897f9333605f8fd8ebf3465e2bb
Raptorkiller <raptorkiller@gmx.at>
2017-04-23 19:48:22 +0200

    Built replay system

---
+++ Assets/MyReplaySystem.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyReplaySystem : MonoBehaviour {
+
+    private const int bufferFrames = 100;
+    private MyKeyFrame[] keyFrames = new MyKeyFrame[bufferFrames];
+    private Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Record();
+    }
+
+    private void PlayBack()
+    {
+        rigidBody.isKinematic = true;
+        int frame = Time.frameCount % bufferFrames;
+        print("Reading frame " + frame);
+        transform.position = keyFrames[frame].position;
+        transform.rotation = keyFrames[frame].rotation;
+    }
+
+    private void Record()
+    {
+        rigidBody.isKinematic = false;
+        int frame = Time.frameCount % bufferFrames;
+        float time = Time.time;
+        print("Writing frame " + frame);
+
+        keyFrames[frame] = new MyKeyFrame(time, transform.position, transform.rotation);
+    }
+}
+
+
+public class MyKeyFrame
+{
+    public float frameTime;
+    public Vector3 position;
+    public Quaternion rotation;
+
+    public MyKeyFrame(float time, Vector3 pos, Quaternion rot) {
+        frameTime = time;
+        position = pos;
+        rotation = rot;
+    }
+}
